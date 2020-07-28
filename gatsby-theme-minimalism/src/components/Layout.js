5a0903bfad5f05523fe0eebd8c715be548871d0a
Andrew Mok <9611545+andrewmmc@users.noreply.github.com>
2020-07-29 03:36:33 +0800

    Update global style

--- gatsby-theme-minimalism/src/components/Layout.js
+++ gatsby-theme-minimalism/src/components/Layout.js
@@ -5,6 +5,22 @@ import { ThemeProvider, CSSReset, Box } from '@chakra-ui/core';
 import { customTheme } from '../themes/styles';
 import Container from './Container';
 
+// https://github.com/chakra-ui/chakra-ui/blob/%40chakra-ui/core%400.8.0/packages/chakra-ui/src/CSSReset/index.js#L6
+const defaultConfig = (theme) => ({
+  light: {
+    color: theme.colors.gray[800],
+    bg: theme.colors.gray[50],
+    borderColor: theme.colors.gray[200],
+    placeholderColor: theme.colors.gray[400],
+  },
+  dark: {
+    color: theme.colors.whiteAlpha[900],
+    bg: theme.colors.gray[800],
+    borderColor: theme.colors.whiteAlpha[300],
+    placeholderColor: theme.colors.whiteAlpha[400],
+  },
+});
+
 const Layout = ({ withContainer = true, children, ...props }) =>
   withContainer ? (
     <Container tabindex="-1" as="main" {...props}>
@@ -17,8 +33,8 @@ const Layout = ({ withContainer = true, children, ...props }) =>
 export const GlobalStyle = ({ children, ...props }) => {
   return (
     <ThemeProvider theme={customTheme} {...props}>
-      <CSSReset />
-      <Box bg="gray.50" minHeight="100vh">
+      <CSSReset config={defaultConfig} />
+      <Box minWidth="min-content" minHeight="100vh">
         {children}
       </Box>
     </ThemeProvider>
